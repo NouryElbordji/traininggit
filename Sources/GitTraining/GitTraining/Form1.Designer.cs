d4feaac95598a8ae289eaec98e35f4b478670c5b
Noureddine ELBORDJI <noureddine.elbordji@ebp.com>
2019-12-03 16:00:07 +0100

    Solution GitTraining

---
+++ Sources/GitTraining/GitTraining/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace GitTraining
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dateEdit1 = new DevExpress.XtraEditors.DateEdit();
+      ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dateEdit1
+      // 
+      this.dateEdit1.EditValue = null;
+      this.dateEdit1.Location = new System.Drawing.Point(170, 145);
+      this.dateEdit1.Name = "dateEdit1";
+      this.dateEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+      this.dateEdit1.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+      this.dateEdit1.Size = new System.Drawing.Size(417, 22);
+      this.dateEdit1.TabIndex = 0;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.dateEdit1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private DevExpress.XtraEditors.DateEdit dateEdit1;
+  }
+}
+
